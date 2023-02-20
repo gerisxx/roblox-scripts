9a2a62646131d07dfe4babee5083733a35c39a11
halloween <107208142+halloweevn@users.noreply.github.com>
2023-02-21 02:15:03 +1100

    Update loadstring.lua

--- DHBountyFarmer/loadstring.lua
+++ DHBountyFarmer/loadstring.lua
@@ -1,1 +1,13 @@
--- // Coming very soon.
+getgenv().Settings = {
+    ["Host"] = 12345, -- // Host ID (Account that inputs commands and collects stomps)
+    ["Attacker"] = 12345, -- // Attacker ID (Account that kills all the ALTs)
+    ["FPS"] = 3, -- // ALTs FPS
+    ["Crew_ID"] = 1, -- // Attacker + Host must be inside of the group
+    ["ALTs"] = { -- // WARNING: These ALTs will lose there bounty
+        12345,
+        12345,
+        12345,
+    },
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/halloweevn/roblox-scripts/main/DHBountyFarmer/src.lua", true))();
