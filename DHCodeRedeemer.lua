9e9fcfa9e50e84a3090c0735266ace20c7be00fb
halloween <107208142+halloweevn@users.noreply.github.com>
2023-02-22 05:54:47 +1100

    Create DHCodeRedeemer.lua

---
+++ DHCodeRedeemer.lua
@@ -0,0 +1,28 @@
+--[[
+  
+  Redeems every code Da Hood ever made
+  Loadstring Version: 
+
+]]--
+
+-- // Await until game is fully loaded
+repeat wait() until game:IsLoaded()
+repeat wait() until game:GetService("Workspace").Players:FindFirstChild(game:GetService("Players").LocalPlayer.Name)
+
+-- // Check game
+if game.PlaceId ~= 2788229376 then
+    game:GetService("Players").LocalPlayer:Kick("ERROR: Script only works inside of Da Hood.")
+    task.wait(3)
+    game:Shutdown()
+end
+
+-- // Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- // Variables
+local Codes = loadstring(game:HttpGet("https://raw.githubusercontent.com/halloweevn/Spooky/main/codes.lua"))()
+
+-- // Redeem every code in the game
+for _, v in pairs(Codes) do
+    ReplicatedStorage.MainEvent:FireServer("EnterPromoCode", Codes)
+end
