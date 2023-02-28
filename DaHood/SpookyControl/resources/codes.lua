2bd5cb2d6c33e397c8318a771ed3131f41c8d5d5
seth <107208142+socialsuicide@users.noreply.github.com>
2023-03-01 10:25:20 +1100

    Update codes.lua

--- DaHood/SpookyControl/resources/codes.lua
+++ DaHood/SpookyControl/resources/codes.lua
@@ -1,1 +1,25 @@
-return "codes"
+-- THIS WILL PROBABLY NOT BE UPDATED NO MORE
+
+local Codes = {
+  "1MLIKES",
+  "@DAHOOD",
+  "militarybase",
+  "LunarNewYear",
+  "MLK",
+  "HappyNewYear2023",
+  "RifleWinter2022",
+  "#HAPPYHALLOWEEN!",
+  "BACK2SCHOOL",
+  "ACCOMPLISHMENT",
+  "AUGUST2022!",
+  "#FREED",
+  "FIREWORKS",
+  "DHSUPRISE!",
+  "2022JUNE",
+  "easterdahood",
+  "Stars",
+  "STARS",
+  "DHUpdate",
+}
+
+return Codes
