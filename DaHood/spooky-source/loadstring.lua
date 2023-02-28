62afb3fc6d1221cad19638190851c2fd9fa3b9d1
seth <107208142+socialsuicide@users.noreply.github.com>
2023-03-01 06:02:26 +1100

    Create loadstring.lua

---
+++ DaHood/spooky-source/loadstring.lua
@@ -0,0 +1,15 @@
+getgenv().Settings = {
+    ["Host"] = 12345, -- // Controller ID
+    ["Prefix"] = "/", -- // Chat Prefix
+    ["FPS"] = 3, -- // Alts FPS
+    ["Advert"] = ".gg/halloweens", -- // Your Advert
+    ["GUI"] = true, -- // GUI Enabled/Disabled
+}
+
+getgenv().Alts = { --// Max is 38
+    Alt1 = 12345,
+    Alt2 = 12345,
+    Alt3 = 12345,
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/socialsuicide/roblox-scripts/main/DaHood/spooky-source/src.lua", true))();
